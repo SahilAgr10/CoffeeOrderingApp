b1d127cc3f2f6fda8adca44e26c8916242e5fb7e
Sahil Agrawal <agrawalsahil4994@gmail.com>
2022-10-02 13:03:19 +0530

    Activity Added

---
+++ app/src/main/java/com/example/CoffeeCafeteria/c3.java
@@ -0,0 +1,14 @@
+package com.example.CoffeeCafeteria;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class c3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_c3);
+    }
+}
